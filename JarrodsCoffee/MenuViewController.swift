b69a0dfadef583d1a47b1231a2336ab32c4e65cd
CarlosDDeLeon <89562671+CarlosDDeLeon@users.noreply.github.com>
2021-11-02 17:49:26 -0700

    Update MenuViewController.swift

--- JarrodsCoffee/MenuViewController.swift
+++ JarrodsCoffee/MenuViewController.swift
@@ -1,21 +1,94 @@
 //
-//  MenuViewController.swift
-//  JarrodsCoffee
+//  ViewController.swift
+//  Jarrods Coffee Builder
 //
-//  Created by Michael Bogner on 6/21/21.
+//  Created by CARLOS DANIEL DE LEON on 11/2/21.
 //
+
 
 import UIKit
 
-class MenuViewController: UIViewController {
+struct JarrodsMenu: Codable {
+    let id: String
+    let menuItem: String
+    let size1: String
+    let size2: String
+    let price1: Double
+    let price2: Double
+}
 
+class ViewController: UIViewController {
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+           super.viewDidLoad()
+        
+        
+        let urlString = "https://raw.githubusercontent.com/CarlosDDeLeon/Coffe-shop-menu/main/data.json"
+                self.loadJson(fromURLString: urlString) { (result) in
+                    switch result {
+                    case .success(let data):
+                        self.parse(jsonData: data)
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+        
+//readJsonFile is the JSON File that Json developed earlier with the menu data.
     }
     
+    private func readLocalFile(forName name: String) -> Data? {
+         do {
+             if let bundlePath = Bundle.main.path(forResource: name,
+                                                  ofType: "json"),
+                 let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                 return jsonData
+             }
+         } catch {
+             print(error)
+         }
+         
+         return nil
+     }
+     
+     private func loadJson(fromURLString urlString: String,
+                           completion: @escaping (Result<Data, Error>) -> Void) {
+         if let url = URL(string: urlString) {
+             let urlSession = URLSession(configuration: .default).dataTask(with: url) { (data, response, error) in
+                 if let error = error {
+                     completion(.failure(error))
+                 }
+                 
+                 if let data = data {
+                     completion(.success(data))
+                 }
+             }
+             
+             urlSession.resume()
+         }
+     }
 
+     private func parse(jsonData: Data) {
+         do {
+             let decodedData = try JSONDecoder().decode(JarrodsMenu.self,
+                                                        from: jsonData)
+             
+             print("id: ", decodedData.id)
+             print("menuItem: ", decodedData.menuItem)
+             print("size1: ", decodedData.size1)
+             print("size2: ", decodedData.size2)
+             print("price1: ", decodedData.price1)
+             print("price2: ", decodedData.price2)
+
+             
+         } catch {
+             print("Decode error, sometingh went wrong.")
+         }
+     }
+    
+    
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
@@ -25,5 +98,4 @@ class MenuViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
